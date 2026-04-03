18ab6784fb4e8a8cf4a94152025d17797c05b269
Samuel Holden <samuelchanceholden@gmail.com>
2026-04-03 16:47:01 -0700

    Player + heuristic creation

--- GameRunner.java
+++ GameRunner.java
@@ -7,12 +7,60 @@ public class GameRunner{
 		Dice gameDice = new Dice();
 		Board gameBoard = new Board(gameDice);
 		ArrayList<Player> players = new ArrayList<Player>();
-		System.out.println("Input amount of players: ");
+		Player user;
+		System.out.println("Input amount of players (including you): ");
 		int playerNumber = input.nextInt();
 		input.nextLine();
+		System.out.println("Now entering agent creator. To instantiate self, type name as: \"self\".");
 		for(int playerCreationIndex = 0; playerCreationIndex < playerNumber;playerCreationIndex++) {
-			System.out.println("Enter the name of player "+(playerCreationIndex+1)+": ");
+			System.out.println("Enter the name of player "+(playerCreationIndex+1)+" (Turn Order): ");
 			players.add(new Player(input.nextLine(),gameBoard,gameDice));
+			if (!players.get(playerCreationIndex).name.equals("self")) {
+				System.out.println("Enter the propertyGroup0Bias of player "+(playerCreationIndex+1)+":");
+				players.get(playerCreationIndex).propertyGroup0Bias = input.nextDouble();
+				input.nextLine();//Eats up the return character
+				System.out.println("Enter the propertyGroup1Bias of player "+(playerCreationIndex+1)+":");
+				players.get(playerCreationIndex).propertyGroup1Bias = input.nextDouble();
+				input.nextLine();
+				System.out.println("Enter the propertyGroup2Bias of player "+(playerCreationIndex+1)+":");
+				players.get(playerCreationIndex).propertyGroup2Bias = input.nextDouble();
+				input.nextLine();
+				System.out.println("Enter the propertyGroup3Bias of player "+(playerCreationIndex+1)+":");
+				players.get(playerCreationIndex).propertyGroup3Bias = input.nextDouble();
+				input.nextLine();
+				System.out.println("Enter the propertyGroup4Bias of player "+(playerCreationIndex+1)+":");
+				players.get(playerCreationIndex).propertyGroup4Bias = input.nextDouble();
+				input.nextLine();
+				System.out.println("Enter the propertyGroup5Bias of player "+(playerCreationIndex+1)+":");
+				players.get(playerCreationIndex).propertyGroup5Bias = input.nextDouble();
+				input.nextLine();
+				System.out.println("Enter the propertyGroup6Bias of player "+(playerCreationIndex+1)+":");
+				players.get(playerCreationIndex).propertyGroup6Bias = input.nextDouble();
+				input.nextLine();
+				System.out.println("Enter the propertyGroup7Bias of player "+(playerCreationIndex+1)+":");
+				players.get(playerCreationIndex).propertyGroup7Bias = input.nextDouble();
+				input.nextLine();
+				System.out.println("Enter the railroadBias of player "+(playerCreationIndex+1)+":");
+				players.get(playerCreationIndex).railroadBias = input.nextDouble();
+				input.nextLine();
+				System.out.println("Enter the utilityBias of player "+(playerCreationIndex+1)+":");
+				players.get(playerCreationIndex).utilityBias = input.nextDouble();
+				input.nextLine();
+				System.out.println("Enter the leaderTradeBias of player "+(playerCreationIndex+1)+":");
+				players.get(playerCreationIndex).leaderTradeBias = input.nextDouble();
+				input.nextLine();
+				System.out.println("Enter the tradeBias of player "+(playerCreationIndex+1)+":");
+				players.get(playerCreationIndex).tradeBias = input.nextDouble();
+				input.nextLine();
+				System.out.println("Enter the jailStayBias of player "+(playerCreationIndex+1)+":");
+				players.get(playerCreationIndex).jailStayBias = input.nextDouble();
+				input.nextLine();
+			}
+			else {
+				players.get(playerCreationIndex).isAgent = false;
+				user = players.get(playerCreationIndex);
+			}
+			
 		}
 		gameBoard.addCommunityChestCard(new GroupPayingCard("GRAND OPERA OPENING",50,players));
 		gameBoard.addChanceCard(new GroupPayingCard("ELECTED CHAIRMAN OF THE BOARD",-50,players));
--- Player.java
+++ Player.java
@@ -11,6 +11,22 @@ public class Player{
     private final Board gameBoard;
     private final Dice gameDice;
     
+    public double propertyGroup0Bias;
+    public double propertyGroup1Bias;
+    public double propertyGroup2Bias;
+    public double propertyGroup3Bias;
+    public double propertyGroup4Bias;
+    public double propertyGroup5Bias;
+    public double propertyGroup6Bias;
+    public double propertyGroup7Bias;
+    
+    public double railroadBias;
+    public double utilityBias;
+    public double leaderTradeBias; // 1.0 is rational, 1.2 is a suck up, 0.8 is looking out for the underdogs
+    public double tradeBias; //1.2 gets scammed, 0.8 is hesitant
+    public double jailStayBias;
+    
+    public Boolean isAgent = true;
 
     public Player(String name, Board gameBoard, Dice gameDice){
         this.name = name;
